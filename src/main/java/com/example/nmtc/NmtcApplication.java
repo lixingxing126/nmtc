32fc926314a09e388a2db6cd01440da84bd16010
栗星星 <lixingxing_lz@126.com>
2020-03-31 15:22:39 +0800

    Initial commit

---
+++ src/main/java/com/example/nmtc/NmtcApplication.java
@@ -0,0 +1,13 @@
+package com.example.nmtc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NmtcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NmtcApplication.class, args);
+    }
+
+}
